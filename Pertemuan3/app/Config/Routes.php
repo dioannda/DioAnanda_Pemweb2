04b3c494e6f2e99bff2914a09361fbb83e78b119
dioannnda <FarhanDzakiIlmiYuliasa@saintek.unipdu.ac.id>
2025-03-19 09:29:04 +0700

    add routes

---
+++ Pertemuan3/app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/About', 'Page::about');
+$routes->get('/Contact', 'Page::about');
+$routes->get('/Faqs', 'Page::faqs');
