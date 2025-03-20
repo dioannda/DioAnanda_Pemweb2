6428aea00833fc3eb6ea08844a96f6f0a8a82cab
dioannda <dioananda275@gmail.com>
2025-03-20 14:56:53 +0700

    add route,controller,page,biodata

---
+++ Pertemuan3/app/Config/Routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::contact');
+$routes->get('/faqs', 'Page::faqs');
+$routes->setAutoRoute(true);
+
