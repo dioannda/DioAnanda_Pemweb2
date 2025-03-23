6ca2950cc25ba9a78106833c849e586afd35d8b8
dioannda <dioananda275@gmail.com>
2025-03-23 14:53:32 +0700

    editing routing dan controller

--- project-root-Dio_Pemweb2/project_root/app/Config/Routes.php
+++ project-root-Dio_Pemweb2/project_root/app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/about', 'Page::about');
+$routes->get('contact', 'Page::contact');
+$routes->get('/faqs', 'Page::faqs');
+$routes->setAutoRoute(true);
+
+
+
---
+++ project-root-Dio_Pemweb2/project_root/app/Controllers/Page.php
@@ -0,0 +1,29 @@
+<?php namespace App\Controllers;
+class Page extends BaseController {
+    public function about()
+    {
+        echo "about page";
+    }
+    public function contact()
+    {
+        echo "contact page";
+    }
+    public function faqs()
+    {
+        echo "Faqs page";
+    }
+    public function tos()
+    {
+        echo "Halaman Term of Service";
+    }
+    public function biodata()
+    {
+        echo "Name              : Dio Ananda Maulana Wijaya";
+        echo "<br>NIM           : 4123031";
+        echo "<br>Alamat        : Jalan Teuku Umar No 70 Jombang";
+        echo "<br>Hobi          : Musik, Olahraga, Game";
+        echo "<br>Pekerjaan     : Mahasiswa";
+        echo "<br>Jenis Kelamin : Laki-Laki";
+        echo "<br>Pendidikan    : Universitas Pondok Pesantren Tinggi Darul Ulum Jombang";
+    }
+}
